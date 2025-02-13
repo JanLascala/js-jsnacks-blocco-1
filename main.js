fe29b1e5a5b08f2473ae013f88958dfb6845c3df
JanLascala <lascala.jan@gmail.com>
2025-02-13 11:34:58 +0100

    Add equal names

--- main.js
+++ main.js
@@ -6,4 +6,8 @@ if (word1.length > word2.length) {
     console.log(word1 + " " + word2 + " " + "first word is longer")
 } else if (word2.length > word1.length) {
     console.log(word2 + " " + word1 + " " + "second word is longer")
+} else if (word2.length = word1.length) {
+    console.log(`names are equal`)
 }
+
+
