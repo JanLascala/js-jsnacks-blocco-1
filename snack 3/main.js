f68b7d576139208db76f25183728cc6ae2934549
JanLascala <lascala.jan@gmail.com>
2025-02-13 12:24:12 +0100

    time up

--- snack 3/main.js
+++ snack 3/main.js
@@ -1,8 +1,8 @@
 
-const number1 = []
+const number = []
 const numberofprompt = 10
 
 for (let i = 0; i < 10; i++) {
     number1.push(prompt("write number"));
+    console.log(number)
 }
-console.log(number1)
