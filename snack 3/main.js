748652fe52203bfe33926c3408402097b00155b9
JanLascala <lascala.jan@gmail.com>
2025-02-13 12:32:01 +0100

    Correction

--- snack 3/main.js
+++ snack 3/main.js
@@ -1,8 +1,11 @@
 
-const number = []
-const numberofprompt = 10
+const numberofprompt = 10;
+let sum = 0;
 
 for (let i = 0; i < 10; i++) {
-    number.push(prompt("write number"));
-    console.log(number)
+    const usernumb = number.push(prompt("write number"));
+    console.log(usernumb)
+    sum = sum + usernumb
+
+
 }
