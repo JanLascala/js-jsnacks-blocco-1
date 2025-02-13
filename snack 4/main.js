104afc456b9633edcfb8457899d7d738809f32b1
JanLascala <lascala.jan@gmail.com>
2025-02-13 13:02:02 +0100

    n

--- snack 4/main.js
+++ snack 4/main.js
@@ -1,9 +1,11 @@
 console.log("say hi")
 const number = []
+
 for (let i = 0; i < 2; i++) {
     const usernumb = number.push(prompt("write number"));
     console.log(usernumb)
-    if (usernumb % 2 === 0) {
+
+    if (usernumb % 2 !== 0) {
         console.log(usernumb + "odd")
     }
 } 
