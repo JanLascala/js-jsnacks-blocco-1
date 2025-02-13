669fe281e5136e2584277447e4638ddeb94b0083
JanLascala <lascala.jan@gmail.com>
2025-02-13 10:28:56 +0100

    snack 1

---
+++ snack 1/main.js
@@ -0,0 +1,9 @@
+
+const number1 = prompt("Insert number 1")
+const number2 = prompt("Insert number 2")
+
+
+if (number1 < number2)
+    console.log(number2)
+else (number2 < number1)
+console.log(number1)
