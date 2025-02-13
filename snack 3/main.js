13d53f9fbfaa1b73fbc050fee1a8a4be47963817
JanLascala <lascala.jan@gmail.com>
2025-02-13 12:21:44 +0100

    Progress!

--- snack 3/main.js
+++ snack 3/main.js
@@ -1,7 +1,8 @@
 
-const number1 = prompt(" Write a number")
+const number1 = []
 const numberofprompt = 10
 
-for (let i = 0; i < 5; i++) {
-    number1.push(prompt());
+for (let i = 0; i < 10; i++) {
+    number1.push(prompt("write number"));
 }
+console.log(number1)
