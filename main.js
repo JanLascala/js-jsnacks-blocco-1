bfbd7182e4f00e24b7e8cb727ef5d4ccacc9b4b1
JanLascala <lascala.jan@gmail.com>
2025-02-13 11:30:06 +0100

    add some text

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const word2 = prompt("insert second word")
 
 
 if (word1.length > word2.length) {
-    console.log(word1 + " " + word2)
+    console.log(word1 + " " + word2 + " " + "first word is longer")
 } else if (word2.length > word1.length) {
-    console.log(word2 + " " + word1)
+    console.log(word2 + " " + word1 + " " + "second word is longer")
 }
