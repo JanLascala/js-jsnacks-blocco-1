732ca3712217c2b0cb1c6480c385b75199f2ae65
JanLascala <lascala.jan@gmail.com>
2025-02-13 10:50:39 +0100

    correction

--- snack 1/main.js
+++ snack 1/main.js
@@ -5,11 +5,7 @@ const number2 = prompt("Insert number 2")
 
 if (number1 < number2) {
     console.log(number2)
-
-    if (number2 < number1) {
-        console.log(number1)
-    }
-
-
+} else if (number2 < number1) {
+    console.log(number1)
 }
 
